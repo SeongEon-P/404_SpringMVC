09166ffa6048163c2cda824f89851901b53e58fb
SeongEon-P <tjddjs909@naver.com>
2024-04-24 09:25:06 +0900

    스프링 b01, Thymeleaf, if문

--- b01/src/test/java/org/zerock/b01/DataSourceTests.java
+++ b01/src/test/java/org/zerock/b01/DataSourceTests.java
@@ -8,23 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 @SpringBootTest
 @Log4j2
 public class DataSourceTests {
-    @Autowired
-    private DataSource dataSource;
+  @Autowired
+  private DataSource dataSource;
 
-    @Test
-    public void testConnection() throws SQLException {
-
-        @Cleanup
-        Connection con = dataSource.getConnection();
-
-        log.info(con);
-
-        Assertions.assertNotNull(con);
-    }
+  @Test
+  public void testConnection() throws SQLException {
+    @Cleanup Connection conn = dataSource.getConnection();
+    @Cleanup PreparedStatement stmt = conn.prepareStatement("SELECT now(),?");
+    stmt.setString(1, "asd");
+    @Cleanup ResultSet rs = stmt.executeQuery();
+    rs.next();
+    log.info(rs.getString(1));
+    log.info(rs.getString(2));
+    Assertions.assertNotNull(conn);
+  }
 }
