09c982ab2686126eea041c5aedafb5ecba90f7e7
박성언 <tjddjs909@naver.com>
2024-05-09 09:20:27 +0900

    Board클래스에 @OneToMany적용, mappedBy를 이용한 구조 변경

--- b01/src/main/java/org/zerock/b01/domain/Board.java
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -3,12 +3,15 @@ package org.zerock.b01.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = "imageSet")
 public class Board extends BaseEntity{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,13 +23,19 @@ public class Board extends BaseEntity{
   @Column(length = 50, nullable = false)
   private String writer;
 
-  // 양방향을 한다면,
+
 
 
   public void change(String title, String content){
     this.title = title;
     this.content = content;
   }
+
+  @OneToMany(mappedBy = "board") //BoardImage의 board변수
+  @Builder.Default
+  //HashSet의 특징은 중복되지 않고, 순서가 없음. 중복되면 삭제됨
+  private Set<BoardImage> imageSet = new HashSet<>();
+  
 }
 
 
