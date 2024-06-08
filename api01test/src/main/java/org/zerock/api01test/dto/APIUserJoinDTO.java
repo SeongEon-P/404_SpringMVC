7b8cea5e9f5defb758c2821d96ce36ba61da94e2
박성언 <tjddjs909@naver.com>
2024-06-08 18:29:20 +0900

    1.커밋과정에서 충돌발생, DTO패키지 이름을 원래대로 변경 MemberJoinDTO 내용수정을 원래대로 되돌림
    2.controller 패키지 생성
    MemberController 추가
    
    3.service 패키지 생성
    MemberService 추가
    MemberServiceImpl 추가
    
    4.domain 패키지 생성
    Memeber 추가
    
    5.build.gradle에
    ModelMapper 추가
    스프링 security, 인증 추가
    
    6.security 패키지 생성
    security/dto 패키지 생성
    MemberSecurityDTO추가
    CustomUserDetailsService추가
    
    -Member 에서 social(kakao로그인, del 관련한건 일단 다 삭제했음. 필요하면 다시 추가)

---
+++ api01test/src/main/java/org/zerock/api01test/dto/APIUserJoinDTO.java
@@ -0,0 +1,19 @@
+package org.zerock.api01test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIUserJoinDTO {
+    private String mid;
+    private String mpw;
+    private String name;
+    private String email;
+    private boolean emailConsent;
+    private boolean snsConsent;
+}
